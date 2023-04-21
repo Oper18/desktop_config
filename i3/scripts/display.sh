bb4110ecd69d62b00dfe2f399eb1427fea5674ce
Grigory <genovikov93@gmail.com>
2023-04-21 23:48:46 +0100

    update display connection listener config

--- i3/scripts/display.sh
+++ i3/scripts/display.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
-#STATUS_OLD=$(cat /sys/class/drm/card0-HDMI-A-1/status)
-#STATUS_NEW=$(cat /sys/class/drm/card0-HDMI-A-1/status)
+declare -A screensmap
 
-STATUS_OLD_HDMI=$(xrandr | grep HDMI | awk '{print $2}' | head -n 1)
-STATUS_NEW_HDMI=$(xrandr | grep HDMI | awk '{print $2}' | head -n 1)
+while IFS= read -r line; do
+  screensmap[$line]=$(xrandr | grep "^${line}" | awk '{print $2}')
+done < <(xrandr | grep "^HDMI" | awk '{print $1}')
+while IFS= read -r line; do
+  screensmap[$line]=$(xrandr | grep "^${line}" | awk '{print $2}')
+done < <(xrandr | grep "^DP" | awk '{print $1}')
 
-STATUS_OLD_DP=$(xrandr | grep "^DP" | awk '{print $2}' | head -n 1)
-STATUS_NEW_DP=$(xrandr | grep "^DP" | awk '{print $2}' | head -n 1)
+echo ${screensmap[@]} ${!screensmap[@]}
+
+check_screen_connection () {
+  for screen in ${!screensmap[@]}
+  do
+    if [[ "${screensmap[$screen]}" == "connected" && "${screensmap[$screen]}" != "$(xrandr | grep "^${screen}" | awk '{print $2}')" ]]; then
+      xrandr --auto
+    elif [[ "${screensmap[$screen]}" == "disconnected" && "${screensmap[$screen]}" != "$(xrandr | grep "^${screen}" | awk '{print $2}')" ]]; then
+      xrandr --auto --output eDP-1 --left-of $screen
+    fi
+    screensmap[$screen]=$(xrandr | grep "^${screen}" | awk '{print $2}')
+  done
+}
 
 while true
 do
-    STATUS_NEW_HDMI=$(xrandr | grep HDMI | awk '{print $2}' | head -n 1)
-    STATUS_NEW_DP=$(xrandr | grep "^DP" | awk '{print $2}' | head -n 1)
-    if [[ "$STATUS_NEW_HDMI" == "disconnected" && "$STATUS_NEW_HDMI" != "$STATUS_OLD_HDMI" ]]; then
-#        echo "disconnected monitor"
-#        xrandr --output eDP-1 --mode 1920x1080
-        xrandr --auto
-	STATUS_OLD_HDMI=$(xrandr | grep HDMI | awk '{print $2}' | head -n 1)
-	sleep 1s
-    elif [[ "$STATUS_NEW_HDMI" == "connected" && "$STATUS_NEW_HDMI" != "$STATUS_OLD_HDMI" ]]; then
-#        echo "connected monitor"
-        xrandr --auto --output eDP-1 --left-of HDMI-1
-	STATUS_OLD_HDMI=$(xrandr | grep HDMI | awk '{print $2}' | head -n 1)
-	sleep 1s
-    elif [[ "$STATUS_NEW_DP" == "disconnected" && "$STATUS_NEW_DP" != "$STATUS_OLD_DP" ]]; then
-#        echo "disconnected monitor"
-#        xrandr --output eDP-1 --mode 1920x1080
-        xrandr --auto
-	STATUS_OLD_DP=$(xrandr | grep "^DP" | awk '{print $2}' | head -n 1)
-	sleep 1s
-    elif [[ "$STATUS_NEW_DP" == "connected" && "$STATUS_NEW_DP" != "$STATUS_OLD_DP" ]]; then
-#        echo "connected monitor"
-        xrandr --auto --output eDP-1 --left-of DP-1
-	STATUS_OLD_DP=$(xrandr | grep "^DP" | awk '{print $2}' | head -n 1)
-	sleep 1s
-    else
-#        echo "no changes"
-	sleep 1s
-    fi
+  check_screen_connection
+  sleep 1s
 done
